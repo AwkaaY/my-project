91afcbe81729793954278d32980b830adc9c5425
AwkaaY <146024142+AwkaaY@users.noreply.github.com>
2024-02-08 17:50:32 +0300

    Add files via upload

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	cout << "Задание 1";
+   cout << "\nТы намекала, но слишком тонко,\n";
+   cout << "Чего-то что за душой держала\n";
+   cout << "Ни разу даже жало не показала\n ";
+   cout << "И вдруг убежала\n";
+   cout << "Но кто же ёлку нарядит теперь мне,\n";
+   cout << "Кто нарисует сердечко в пене?\n";
+   cout << "Кто будет пахнуть духами вкусно,\n";
+   cout << "И с кем подтяну свой английский устный?\n\n";
+   cout << "\t\t\t\tЗилков AE\n\n";
+   cout << "Задание 2\n";
+   cout << "Every\n";
+   cout << "\thunter\n";
+   cout << "\t\twants\n";
+   cout << "\t\t\tto know\n";
+   cout << "\t\t\t\twhere\n";
+   cout << "\t\t\t\t\tDoes pheasant\n";
+   cout << "\t\t\t\t\t\tsit\n";
+   cout << "Задание 3";
+   cout << "\t\t\t ПРОДАЮ ГИТАРУ\n";
+   cout << "\t\t\t Вид:Акустическая\n";
+   cout << "\t\t Почти новая,строит,играется легко\n";
+   cout << "\t\t\tЗонить по номеру:\n";
+   cout << "\t\t\t333-77-77\n";
+   cout << "\t\t\t\t\tЕгор Попов\n";
+
+}
