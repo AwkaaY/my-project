1d43c0d79e01e34295e4967be0b856819ace6ab1
AwkaaY <146024142+AwkaaY@users.noreply.github.com>
2024-02-08 17:46:38 +0300

    Add files via upload

---
+++ Para/Para/Para.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>             //Подключаемая библиотека
+//using namespace std;
+
+int main()                  //Определение функции Main
+{
+    char sym = 'E';
+    char sym1{ 'g' };
+    char sym3 = 'o';
+    char sym4 = 'r';
+
+    int letter1 = sym;
+    int letter2 = sym1;
+    int letter3 = sym3;
+    int letter4 = sym4;
+    std::cout << letter1 << " " << letter2 << " " << " " << letter3 << " " << letter4;
+}
